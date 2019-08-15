92217fe874cd9f78cb97aee30304ae6e189cb5d6
paul magu <pmagu@yahoo.com>
2019-08-14 17:24:23 -0700

    nth Commit (def msg)

--- appGitUpdate.cmd
+++
@@ -1,9 +0,0 @@
-rem echo off
-
-if %1% EQU "" Set Msg="first Commit"
-if %1% NEQ "" Set Msg=%1%
-git add .
-git commit -m %msg%
-echo Message: %msg%
-rem git remote add origin https://github.com/ipaulmagu/testexpress2.git
-rem git push -u origin master
--- appGitUpdate.ps1
+++ appGitUpdate.ps1
@@ -1,3 +1,5 @@
 ﻿param($msg="nth Commit (def msg)")
 git add .
 git commit -m $msg
+git remote add origin https://github.com/ipaulmagu/testexpress2.git
+git push -u origin master
